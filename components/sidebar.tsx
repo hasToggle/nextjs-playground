3695072285b1765c0bd34aee569893ebe637475b
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-05 20:56:39 +0200

    adjust topic

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,7 +14,7 @@ export function Sidebar({ className, playlists }: SidebarProps) {
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            The Component
+            React Basics
           </h2>
           <div className="space-y-1">
             <Button variant="secondary" className="w-full justify-start">
@@ -31,7 +31,7 @@ export function Sidebar({ className, playlists }: SidebarProps) {
                 <circle cx="12" cy="12" r="10" />
                 <polygon points="10 8 16 12 10 16 10 8" />
               </svg>
-              Lifecycle
+              Component Lifecycle
             </Button>
             <Button variant="ghost" className="w-full justify-start">
               <svg
