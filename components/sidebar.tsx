c08b07d675d595f95da0a46ba1436d5ccded1251
Ernst Stolz <eric@spiritsdontfly.com>
2024-07-18 14:49:24 +0200

    adjust titles

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -33,6 +33,25 @@ export function Sidebar({ className, playlists }: SidebarProps) {
                 strokeLinejoin="round"
                 className="mr-2 h-4 w-4"
               >
+                <path d="M21 15V6" />
+                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                <path d="M12 12H3" />
+                <path d="M16 6H3" />
+                <path d="M12 18H3" />
+              </svg>
+              JSX
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
                 <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
                 <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
                 <circle cx="12" cy="12" r="2" />
@@ -87,9 +106,49 @@ export function Sidebar({ className, playlists }: SidebarProps) {
         </div>
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Data Fetching
+            React Server Components
           </h2>
           <div className="space-y-1">
+            <Button
+              onClick={() => router.push("/products/ssr")}
+              variant="ghost"
+              className="w-full justify-start"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+              Data Fetching
+            </Button>
+            <Button
+              onClick={() => router.push("/products/ssg")}
+              variant="ghost"
+              className="w-full justify-start"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="8" cy="18" r="4" />
+                <path d="M12 18V2l7 4" />
+              </svg>
+              Composition Patterns
+            </Button>
             <Button
               onClick={() => router.push("/products/csr")}
               variant="ghost"
@@ -112,46 +171,6 @@ export function Sidebar({ className, playlists }: SidebarProps) {
                 <path d="M12 18H3" />
               </svg>
               CSR
-            </Button>
-            <Button
-              onClick={() => router.push("/products/ssg")}
-              variant="ghost"
-              className="w-full justify-start"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="8" cy="18" r="4" />
-                <path d="M12 18V2l7 4" />
-              </svg>
-              SSG - Static
-            </Button>
-            <Button
-              onClick={() => router.push("/products/ssr")}
-              variant="ghost"
-              className="w-full justify-start"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
-              SSR - Made for You
             </Button>
             <Button
               onClick={() => router.push("/products/suspense-in-parallel")}
@@ -199,7 +218,7 @@ export function Sidebar({ className, playlists }: SidebarProps) {
         </div>
         <div className="py-2">
           <h2 className="relative px-7 text-lg font-semibold tracking-tight">
-            Forms
+            Server Actions
           </h2>
           <ScrollArea className="h-[300px] px-1">
             <div className="space-y-1 p-2">
--- lib/playlists.ts
+++ lib/playlists.ts
@@ -1,16 +1,3 @@
 export type Playlist = (typeof playlists)[number];
 
-export const playlists = [
-  "Server Actions",
-  "Recently Played",
-  "Top Songs",
-  "Top Albums",
-  "Top Artists",
-  "Logic Discography",
-  "Bedtime Beats",
-  "Feeling Happy",
-  "I miss Y2K Pop",
-  "Runtober",
-  "Mellow Days",
-  "Eminem Essentials",
-];
+export const playlists = ["Forms", "Buttons", "useEffect"];
